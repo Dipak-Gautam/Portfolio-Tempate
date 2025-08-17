a4ab47af54c45377a33f5e2543f9e3cd15904b77
Dipak-Gautam <anjangautam095@gmail.com>
2025-08-17 16:00:44 +0545

    git conflict fixed

---
+++ src/Data/workData.js
@@ -0,0 +1,21 @@
+const workData = [
+  {
+    projectName: "Food Pokhara",
+    image: "./my/work/food-ecommerce.png",
+    projectDescription:
+      "FoodPokhara is a MERN-stack e-commerce platform with features like a shopping cart, role-based access, order management, and a CMS to easily add, update, or delete products.",
+    tags: ["react", "express", "node,js", "mongoDb"],
+    gitHubLink: "https://github.com/Dipak-Gautam/BrahmaRupaOjt",
+    projectLink: "https://foodpokhara.netlify.app/",
+  },
+  {
+    projectName: "Cat Rental",
+    image: "./my/work/carRental.png",
+    projectDescription:
+      "A modern car rental platform with features like booking management, role-based access, vehicle inventory control, and a CMS to add, update, or remove cars with ease.",
+    tags: ["react", "tailwind css", "react icons"],
+    gitHubLink: "https://github.com/Dipak-Gautam/react-Car-Rental-webapp",
+    projectLink: "https://mero-car-rental.netlify.app/",
+  },
+];
+export default workData;
